22a6b57b77f9539127d7bd633c6deb40aa76f951
sibani97 <sibani.iem19@gmail.com>
2019-06-05 19:47:32 +0530

    todaytask

--- src/app/component/app-icon/app-icon.component.ts
+++ src/app/component/app-icon/app-icon.component.ts
@@ -13,9 +13,10 @@ import { CollaboratorDialogComponent } from '../collaborator-dialog/collaborator
 })
 export class AppIconComponent implements OnInit {
   
-   allLabels:any[]; 
+  allLabels:any[]; 
   items:any;
   labelOfNotes:any[];
+  
   @Input() noteData: any;
 
   // allLabels:any;
@@ -25,7 +26,57 @@ export class AppIconComponent implements OnInit {
   untrash:any;
   unarchive:any;
   labelName=new FormControl('');
- 
+  addColour=new FormControl('');
+  arrayOfColors= [
+    [
+    { colorName: "LightCoral", colorCode: "#F08080" },
+    { colorName: "Red", colorCode: "#FF0000" },
+    { colorName: "Crimson", colorCode: "#DC143C" },
+    { colorName: "FireBrick", colorCode: "#B22222" },
+    ],
+    [
+    { colorName: "HotPink", colorCode: "#FF69B4" },
+    { colorName: "DeepPink", colorCode: "#FF1493" },
+    { colorName: "PaleVioletRed", colorCode: "#DB7093" },
+    { colorName: "dark blue", colorCode: "#0000A0" },
+    ],
+    [
+    { colorName: "Coral", colorCode: "#FF7F50" },
+    { colorName: "OrangeRed", colorCode: "#FF4500" },
+    { colorName: "Tomato", colorCode: "#FF6347" },
+    { colorName: "Yellow", colorCode: "#FFFF00" },
+    ],
+    [
+      { colorName: "Violet", colorCode: "#EE82EE" },
+      { colorName: "Fuchsia", colorCode: "#FF00FF" },
+      { colorName: "BlueViolet", colorCode: "#8A2BE2" },
+      { colorName: "DarkViolet", colorCode: "#9400D3" },
+      ],
+      [
+        { colorName: "Purple", colorCode: "#800080" },
+        { colorName: "MediumSlateBlue", colorCode: "#7B68EE" },
+        { colorName: "GreenYellow", colorCode: "#ADFF2F" },
+        { colorName: "Lime", colorCode: "#00FF00" },
+        ],
+        [
+          { colorName: "MediumSpringGreen", colorCode: "#00FA9A" },
+          { colorName: "MediumSlateBlue", colorCode: "#7B68EE" },
+          { colorName: "Olive", colorCode: "#808000" },
+          { colorName: "LightSeaGreen", colorCode: "#20B2AA" },
+          ],
+          [
+            { colorName: "Aqua", colorCode: "#00FFFF" },
+            { colorName: "DeepSkyBlue", colorCode: "#00BFFF" },
+            { colorName: "DodgerBlue", colorCode: "#1E90FF" },
+            { colorName: "CornflowerBlue", colorCode: "#6495ED" },
+            ] ,
+            [
+              { colorName: "Brown", colorCode: "#A52A2A" },
+              { colorName: "RosyBrown", colorCode: "#BC8F8F" },
+              { colorName: "DarkSlateGray", colorCode: "#2F4F4F" },
+              { colorName: "Khaki", colorCode: "#F0E68C" },
+              ]  
+    ];
   constructor(private noteService: NoteService, private snackbar: MatSnackBar,public dialog: MatDialog,private labelService:LabelService) { }
 
   ngOnInit() {
@@ -36,6 +87,7 @@ console.log("Collaborator"+this.noteData.noteId);
     this.getUnPinned();
     this.getUnArchive();
     this.getallLabel();
+    console.log("Colors",this.arrayOfColors);
     // this.getUnTrash();
 
   }
@@ -213,10 +265,29 @@ openDialoglabelCollaboretor(items): void
 
   dialogRef.afterClosed().subscribe(result => {
     console.log('dialog box closed');
- 
+   })
 
-  })
+}
 
+
+addColourToNote(items)
+{
+  this.items={
+"addColor":this.addColour.value
+ };
+console.log("colour ssuccessfully set")
+this.noteService.postRequest("/note/addColor?noteId="+this.noteData.noteId+"&colour"+items.addColor,null)
+.subscribe((response:any)=>{
+if(response.statusCode===200){
+console.log(response);
+this.snackbar.open("colour is successfully added","undo",{duration:2500});
+  }
+  else{
+    console.log("colour is not added");
+    this.snackbar.open("colour is not added","undo",{duration:2500});
+    // this.snackbar.open()
+  }
+});
 }
 
 
